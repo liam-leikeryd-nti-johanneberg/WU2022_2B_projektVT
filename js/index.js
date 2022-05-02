6ccee11aa28647ee99848afe6a7c05f71cc23588
Liam <95085039+liam-leikeryd-nti-johanneberg@users.noreply.github.com>
2022-05-02 14:49:32 +0200

    started picture slide

--- js/index.js
+++ js/index.js
@@ -7,3 +7,38 @@ function toggleMenu() {
 }
 
 burger.addEventListener('click', toggleMenu)
+
+
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//btn listeners
+
+nextBtn.addEventListener('click', ()=> {
+    if(counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', ()=> {
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    console.log('Fired');
+});
+
+
