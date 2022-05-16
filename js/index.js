9a8dbb386ca73284ec3b498418f9173d03c657e8
Liam <95085039+liam-leikeryd-nti-johanneberg@users.noreply.github.com>
2022-05-16 13:14:47 +0200

    dunno

--- js/index.js
+++ js/index.js
@@ -25,20 +25,32 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 //btn listeners
 
 nextBtn.addEventListener('click', ()=> {
-    if(counter <= 0) return;
+    if(counter >= carouselImages.length -1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 prevBtn.addEventListener('click', ()=> {
+    if(counter <= 0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 carouselSlide.addEventListener('transitionend', ()=>{
-    if(carouselImages[counter].id ==='lastClone')
+    if(carouselImages[counter].id ==='lastClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length -2 ; 
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    }
+    if(carouselImages[counter].id ==='firstClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter ; 
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    }
 });
 
 
